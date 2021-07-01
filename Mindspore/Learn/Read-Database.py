118f09e103b92c631694146204f2451c96226081
CHTO <Mr_CHTO@outlook.com>
2021-07-02 00:11:48 +0800

    VS0702

--- Mindspore/Learn/Read-Database.py
+++
@@ -1,6 +0,0 @@
-import mindspore.dataset as mds
-data_path = "C:\\GitBox\\py_box\\Mindspore\\Learn\\datasets\\MNIST_Data\\train"
-minist_ds = mds.MindDataset(data_path)
-for a in minist_ds:
-    print(a, end=" ")
-    break
---
+++ Mindspore/Learn/Read-Datasets.py
@@ -0,0 +1,3 @@
+import mindspore.dataset as ds
+data_path = "./datasets/MNIST_Data/train"
+minist_ds = ds.MindDataset(data_path)
