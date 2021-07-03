99adcd2ed67346e45d2353119bab1cb27dd69b7b
CHTO <Mr_CHTO@outlook.com>
2021-07-03 19:59:20 +0800

    VS0703

---
+++ Kaggle/Titanic/Titanic.py
@@ -0,0 +1,5 @@
+import pandas as pd
+path = './Datasets/train.csv'
+data = pd.read_csv(path)
+# data.describe()
+data.columns
--- Mindspore/Learn/Read-Datasets.py
+++ Mindspore/Learn/Read-Datasets.py
@@ -1,3 +1,6 @@
 import mindspore.dataset as ds
-data_path = "./datasets/MNIST_Data/train"
+data_path = './datasets/MNIST_Data/train/train-images.idx3-ubyte'
 minist_ds = ds.MindDataset(data_path)
+for data in minist_ds:
+    print(data)
+    break;
