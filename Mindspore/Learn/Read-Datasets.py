f7fbdb798812f78ebfe15e7a56c77e1b11d6693d
MrCHTO <mr_chto@outlook.com>
2021-07-15 09:27:23 +0000

    VIM0715

--- Mindspore/Learn/Read-Datasets.py
+++ Mindspore/Learn/Read-Datasets.py
@@ -1,6 +1,3 @@
 import mindspore.dataset as ds
 data_path = './datasets/MNIST_Data/train/train-images.idx3-ubyte'
 minist_ds = ds.MindDataset(data_path)
-for data in minist_ds:
-    print(data)
-    break;
